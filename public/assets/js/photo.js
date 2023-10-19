a491395379b53548d59f3fd9ea65b2d9ff752f15
Keaton Burleson <keaton@vipindoorstorage.com>
2023-10-19 17:06:56 -0500

    iOSucks

--- public/assets/js/photo.js
+++ public/assets/js/photo.js
@@ -1,12 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
     const imgElement = document.querySelector('img')
 
-    imgElement.classList.add('hide');
+    if (!imgElement.complete)
+        imgElement.classList.add('hide');
 
     imgElement.onload = imgElement.onerror = () => {
         imgElement.classList.remove('hide');
         imgElement.classList.add('show');
     };
 
-
+    if (imgElement.complete) {
+        imgElement.classList.remove('hide');
+        imgElement.classList.add('show');
+    }
 });
